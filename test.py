14521f7b76d780e1cf049323ca6f10baf0aeedee
Vykstorm <victorruizgomezdev@gmail.com>
2019-04-09 12:02:38 +0200

    Added test script

---
+++ test.py
@@ -0,0 +1,25 @@
+'''
+This script is a simple test for opencv. It should work fine if you installed
+the library correctly on your system
+
+It creates a tv random noise effect
+'''
+
+from time import time
+from numpy import uint8
+from numpy.random import rand
+import cv2
+
+
+
+xy=(512,512)
+Nf = 500
+
+def noisytv():
+    imgs = (rand(Nf,xy[0],xy[1])*255).astype(uint8)
+    while True:
+        for i in imgs:
+            cv2.imshow('test',i)
+            cv2.waitKey(3) #integer milliseconds, 0 makes wait forever
+
+noisytv()
