f7807dbde01bf1f03f55c327c29e13537e8fe8c6
Vykstorm <victorruizgomezdev@gmail.com>
2019-04-23 18:48:42 +0200

    Now argument t has a default value

--- utils/input.py
+++ utils/input.py
@@ -1,9 +1,9 @@
 
 import cv2 as cv
 
-def waitKey(t):
+def waitKey(t=-1):
     '''
     This method is the same as OpenCV waitKey() method, but it returns
     a char object instead of an integer
     '''
-    return chr(waitKey(t) & 255)
+    return chr(cv.waitKey(t) & 255)
